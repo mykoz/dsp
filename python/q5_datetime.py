9f41e0051a2e5395736ccd677a2f6b993c1ca1c0
mykoz <mykoz@users.noreply.github.com>
2015-09-07 11:36:18 -0400

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,31 @@
 # Hint:  use Google to find python function
 
-####a) 
+####a)
+
+import datetime
+
 date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_stop = '07-28-2015'
+
+start = datetime.datetime.strptime(date_start, '%m-%d-%Y')
+end = datetime.datetime.strptime(date_stop, '%m-%d-%Y')   
+print end - start #937 days
 
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+
+start = datetime.datetime.strptime(date_start, '%m%d%Y')
+end = datetime.datetime.strptime(date_stop, '%m%d%Y')   
+print end - start #513 days
+
+
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+
+start = datetime.datetime.strptime(date_start, '%d-%b-%Y')
+end = datetime.datetime.strptime(date_stop, '%d-%b-%Y')   
+print end - start #7850 days

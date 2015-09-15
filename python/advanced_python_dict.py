a769e29ab3ff9d5135aec17fb14c91c1b0267f78
mykoz <mykoz@users.noreply.github.com>
2015-09-15 06:36:17 -0400

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,60 @@
-PLACE YOUR CODE HERE
+#PLACE YOUR CODE HERE
+
+import csv
+import re
+from pprint import pprint 
+from collections import OrderedDict
+
+
+
+
+#Q6  CREATE DICTIONARY
+facdict = {}
+f = open("faculty.csv", "rb")
+faculty = csv.reader(f)
+for row in faculty:
+    last =  row[0].split(' ')[-1]
+    values = [[row[1], row[2], row[3]]]
+    if last in facdict:
+        facdict[last].append(values)
+    else:
+        facdict[last] = values
+    
+pprint (facdict.items()[0:3])
+
+
+
+#Q7 TUPLE NAME
+facdict = {}
+f = open("faculty.csv", "rb")
+faculty = csv.reader(f)
+for row in faculty:
+    last =  row[0].split(' ')
+    nametup = (last[0],last[-1])
+    values = [[row[1], row[2], row[3]]]
+    if nametup in facdict:
+        facdict[nametup].append(values)
+    else:
+        facdict[nametup] = values
+    
+pprint (facdict.items()[0:3])
+
+
+
+#Q8 REVERSE SORT
+facdict = {}
+f = open("faculty.csv", "rb")
+faculty = csv.reader(f)
+next(faculty)
+for row in sorted(faculty):
+    last =  row[0].split(' ')
+    nametup = (last[0],last[-1])
+    values = [[row[1], row[2], row[3]]]
+    if nametup in facdict:
+        facdict[nametup].append(values)
+    else:
+        facdict[nametup] = values
+    
+pprint(sorted(facdict.items()[0:3], key =lambda key:key[0][1],reverse =True))
+#pprint(sorted(facdict.items(), key =lambda key:key[0][1],reverse =True)) #full print is ordered
+#indexing here doesn't work well because dictionaries are inherently unordered so the first 3 are from the middle
